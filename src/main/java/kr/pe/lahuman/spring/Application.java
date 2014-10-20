f77b81bf8370e988c716dd4fc7371ab4e41356d6
lahuman <lahuman1@gmail.com>
2014-10-20 17:55:21 +0900

    UTF-8 Filter & ViewResolver

--- src/main/java/kr/pe/lahuman/spring/Application.java
+++ src/main/java/kr/pe/lahuman/spring/Application.java
@@ -4,29 +4,61 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Created by lahuman on 2014-10-14.
  */
+@EnableWebMvc
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
-public class Application {
+public class Application  extends WebMvcConfigurerAdapter {
     static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        log.info("Let's inspct the bens provider by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(new Class[] {Application.class}, args);
+        log.info("Let's inspct the bens provider by Spring Boot:");
+//
+//        String[] beanNames = ctx.getBeanDefinitionNames();
+//        Arrays.sort(beanNames);
+//        for (String beanName : beanNames) {
+//            log.info(beanName);
+//        }
+    }
+
+
+
+
 }
--- src/main/java/kr/pe/lahuman/spring/hello/HelloController.java
+++ src/main/java/kr/pe/lahuman/spring/hello/HelloController.java
@@ -2,22 +2,29 @@ package kr.pe.lahuman.spring.hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by lahuman on 2014-10-14.
  */
-@RestController
+@Controller
 public class HelloController {
     static final Logger log = LoggerFactory.getLogger(HelloController.class);
     @RequestMapping("/")
-    public String index() {
+    public @ResponseBody String index() {
         log.trace("This is TRACE Log!");
         log.debug("This is DEBUG Log!");
         log.info("This is INFO Log!");
         log.warn("This is WARN Log!");
         log.error("This is ERROR Log!");
         return "Greeting from Sping Boot";
+    }
+
+    @RequestMapping("/test.do")
+         public String helloWorld(){
+        return "test";
     }
 }

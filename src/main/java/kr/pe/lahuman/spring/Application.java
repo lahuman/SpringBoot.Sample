0d9548eb9e714b6759cb4fe979e9a040acd8aa73
lahuman <lahuman1@gmail.com>
2014-10-24 15:39:34 +0900

    국제화 MessageSource 설정

--- src/main/java/kr/pe/lahuman/spring/Application.java
+++ src/main/java/kr/pe/lahuman/spring/Application.java
@@ -6,14 +6,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
 
 /**
  * Created by lahuman on 2014-10-14.
@@ -48,6 +58,7 @@ public class Application  extends WebMvcConfigurerAdapter {
     }
 
     public static void main(String[] args) {
+        //Spring Boot 기동
         ApplicationContext ctx = SpringApplication.run(new Class[] {Application.class}, args);
         log.info("Let's inspct the bens provider by Spring Boot:");
 //
@@ -59,6 +70,37 @@ public class Application  extends WebMvcConfigurerAdapter {
     }
 
 
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        //WEB-INF 밑에 해당 폴더에서 properties를 찾는다.
+        messageSource.setBasename("messages/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
 
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor=new LocaleChangeInterceptor();
+        //request로 넘어오는 language parameter를 받아서 locale로 설정 한다.
+        localeChangeInterceptor.setParamName("language");
+        return localeChangeInterceptor;
+    }
 
+    @Bean(name = "localeResolver")
+    public LocaleResolver sessionLocaleResolver(){
+        //세션 기준으로 로케일을 설정 한다.
+        SessionLocaleResolver localeResolver=new SessionLocaleResolver();
+        //쿠키 기준(세션이 끊겨도 브라우져에 설정된 쿠키 기준으로)
+//        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+
+        //최초 기본 로케일을 강제로 설정이 가능 하다.
+        localeResolver.setDefaultLocale(new Locale("en_US"));
+        return localeResolver;
+    }
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        //Interceptor를 추가 한다.
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }
--- src/main/java/kr/pe/lahuman/spring/hello/HelloController.java
+++ src/main/java/kr/pe/lahuman/spring/hello/HelloController.java
@@ -2,10 +2,16 @@ package kr.pe.lahuman.spring.hello;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
 
 /**
  * Created by lahuman on 2014-10-14.
@@ -13,8 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class HelloController {
     static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @Autowired
+    private LocaleResolver localeResolver;
+
     @RequestMapping("/")
-    public @ResponseBody String index() {
+    public @ResponseBody String index(HttpServletRequest request) {
+
+        String usMessage = messageSource.getMessage("hello.test", null, "no surch", Locale.US);
+        String korMessage = messageSource.getMessage("hello.test",null, "no surch", Locale.KOREA);
+        String localeMessage = messageSource.getMessage("hello.test",null, "no surch", localeResolver.resolveLocale(request));
+
+        log.debug(korMessage);
+        log.debug(usMessage);
+        log.debug(localeMessage);
+
         log.trace("This is TRACE Log!");
         log.debug("This is DEBUG Log!");
         log.info("This is INFO Log!");
@@ -24,7 +46,15 @@ public class HelloController {
     }
 
     @RequestMapping("/test.do")
-         public String helloWorld(){
+    public String helloWorld(HttpServletRequest request, HttpServletResponse response){
+        String usMessage = messageSource.getMessage("hello.test", null, "no surch", Locale.US);
+        String korMessage = messageSource.getMessage("hello.test",null, "no surch", Locale.KOREA);
+        String localeMessage = messageSource.getMessage("hello.test",null, "no surch", localeResolver.resolveLocale(request));
+
+        log.debug("KR: " + korMessage);
+        log.debug("US: " + usMessage);
+        log.debug("USER SET: " + localeMessage);
+        log.debug("response SET: " + response.getLocale().toString());
         return "test";
     }
 }
